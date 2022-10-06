ac0c784caef709801f4a55f52e545b28b0aa3429
Duncun Kiprop <dankiproh@gmail.com>
2022-10-06 23:38:19 +0300

    Notes App

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,16 @@
+import {MdDeleteForever} from "react-icons/md";
+
 const Note = () => {
-    return <div className="note"></div>;
-}
+    return (
+    <div className="note">
+               <span> Hello this is my first note! Hurray</span>
+               <div className="note-footer">
+                   <small>13/07/2022</small>
+                   <MdDeleteForever className= "delete-icon" size="1.3em"/>
+                </div>
+
+    </div>
+    );
+};
 
 export default Note;
